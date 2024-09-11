79bf1b46d36cd6c668572dbcd1c7034d74c76e92
Cecilia <cecilmutero66@gmail.com>
2024-09-11 04:40:47 +0300

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+    this._sqft = 0;
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value === 'number') {
+      this._sqft = value;
+    } else {
+      throw new TypeError('sqft must be a number');
+    }
+  }
+}
