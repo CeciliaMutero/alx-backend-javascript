cfef80a9cd7a0689f9fb0e82ee847a38d8e0ff33
Cecilia <cecilmutero66@gmail.com>
2024-09-13 05:27:53 +0300

    6. Set data structure

---
+++ 0x03-ES6_data_manipulation/6-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/extensions
+import setFromArray from './6-set.js';
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
